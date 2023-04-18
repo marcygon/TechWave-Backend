8fd089af1d26938706e781cd32df20d47973faa8
marcygon <marciasofia49@gmail.com>
2023-04-18 12:03:29 +0200

    'registerToEventModel'

---
+++ src/main/java/com/techevents/app/domain/Models/RegisterToEvent.java
@@ -0,0 +1,22 @@
+package com.techevents.app.domain.Models;
+
+import com.techevents.app.security.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class RegisterToEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}

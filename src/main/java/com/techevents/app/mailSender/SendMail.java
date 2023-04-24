204879311c55af2bf5396b754a774824c203604c
marcygon <marciasofia49@gmail.com>
2023-04-24 16:35:07 +0200

    sendMailToLoggedUser

--- src/main/java/com/techevents/app/mailSender/SendMail.java
+++ src/main/java/com/techevents/app/mailSender/SendMail.java
@@ -1,5 +1,6 @@
 package com.techevents.app.mailSender;
 
+import com.techevents.app.security.auth.AuthenticationService;
 import com.techevents.app.security.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class SendMail {
     @Autowired
     private JavaMailSender mail;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @PostMapping("sendMail")
     public ResponseEntity<?> sendMail(){
+        String getUserMail = authenticationService.getAuthUser().getEmail();
         SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo("marciasofia49@gmail.com");
+        email.setTo(getUserMail);
         email.setFrom("mailsendersfirsttry@gmail.com");
         email.setSubject("Enviado desde backend");
         email.setText("Hola jeje soy un mensaje");

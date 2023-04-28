617d6701ec1ef4477e859889e5458f44b125022b
marcygon <marciasofia49@gmail.com>
2023-04-28 10:40:29 +0200

    'mailDeleted'

--- src/main/java/com/techevents/app/domain/Services/SendMailService.java
+++ src/main/java/com/techevents/app/domain/Services/SendMailService.java
@@ -21,7 +21,7 @@ public class SendMailService {
     public void sendMail(String email){
         SimpleMailMessage emailMessage = new SimpleMailMessage();
         emailMessage.setTo(email);
-        emailMessage.setFrom("mailsendersfirsttry@gmail.com");
+        emailMessage.setFrom("mail@mail.com");
         emailMessage.setSubject("Thank You for Trusting Eventia - Confirmation of Registration");
         emailMessage.setText("Congratulations! You have successfully registered for the event. We look forward to seeing you there and hope you have a great experience. Thank you for your participation!");
 
